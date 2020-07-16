a06ca7798eefdbea6f437e90fc68f19c70f1cc4b
liushuwei <liushuwei@kuaishou.com>
2020-07-16 14:23:32 +0800

    增加创建操作符

--- app/src/main/java/com/lsw/demo/rxjavademo/MainActivity.java
+++ app/src/main/java/com/lsw/demo/rxjavademo/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
+
+/**
+ * 参考文档：https://www.jianshu.com/p/a406b94f3188
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "RxJava";
@@ -22,7 +32,277 @@ public class MainActivity extends AppCompatActivity {
 
         doSubscribe();//直接使用
         doSubscribe1();//基于事件流的链式调用方式
+        doJustSubscribe();//快速创建，只能发送10个参数
+        doFromArraySubscribe();//直接发送传入的数组数据
+        doFromIterableSubscribe();//直接发送传入的集合List数据
+        doDefer();//延迟创建
+        doTime();//延迟指定时间后，调用一次 onNext
+//        doInterval();//每隔指定时间就发送事件,从0开始、无限递增1的的整数序列
+        doIntervalRange();//每隔指定时间就发送事件，可指定发送的数据的数量
+        doRange();//连续发送 1个事件序列，可指定范围(同类的有rangeLong())
+    }
 
+    private void doRange() {
+        // 参数说明：
+        // 参数1 = 事件序列起始点；
+        // 参数2 = 事件数量；
+        // 注：若设置为负数，则会抛出异常
+        Observable.range(3,10)
+                // 该例子发送的事件序列特点：从3开始发送，每次发送事件递增1，一共发送10个事件
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+                    // 默认最先调用复写的 onSubscribe（）
+
+                    @Override
+                    public void onNext(Integer value) {
+                        Log.d(TAG, "接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+
+                });
+
+    }
+
+    private void doIntervalRange() {
+        // 参数说明：
+        // 参数1 = 事件序列起始点；
+        // 参数2 = 事件数量；
+        // 参数3 = 第1次事件延迟发送时间；
+        // 参数4 = 间隔时间数字；
+        // 参数5 = 时间单位
+        Observable.intervalRange(3,10,2, 1, TimeUnit.SECONDS)
+                // 该例子发送的事件序列特点：
+                // 1. 从3开始，一共发送10个事件；
+                // 2. 第1次延迟2s发送，之后每隔2秒产生1个数字（从0开始递增1，无限个）
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+                    // 默认最先调用复写的 onSubscribe（）
+
+                    @Override
+                    public void onNext(Long value) {
+                        Log.d(TAG, "接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+
+                });
+    }
+
+    private void doInterval() {
+        // 参数说明：
+        // 参数1 = 第1次延迟时间；
+        // 参数2 = 间隔时间数字；
+        // 参数3 = 时间单位；
+        Observable.interval(3,1,TimeUnit.SECONDS)
+                // 该例子发送的事件序列特点：延迟3s后发送事件，每隔1秒产生1个数字（从0开始递增1，无限个）
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+                    // 默认最先调用复写的 onSubscribe（）
+
+                    @Override
+                    public void onNext(Long value) {
+                        Log.d(TAG, "接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+
+                });
+
+        // 注：interval默认在computation调度器上执行
+        // 也可自定义指定线程调度器（第3个参数）：interval(long,TimeUnit,Scheduler)
+    }
+
+    private void doTime() {
+        // 该例子 = 延迟2s后，发送一个long类型数值
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(Long value) {
+                        Log.d(TAG, "接收到了事件"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+
+                });
+    }
+
+    private void doDefer() {
+//         1. 第1次对i赋值
+                final Integer i = 10;
+
+        // 2. 通过defer 定义被观察者对象
+        // 注：此时被观察者对象还没创建
+        Observable<Integer> observable = Observable.defer(new Callable<ObservableSource<? extends Integer>>() {
+            @Override
+            public ObservableSource<? extends Integer> call() throws Exception {
+                return Observable.just(i);
+            }
+        });
+
+        Log.i(TAG, "doDefer: i = "+i);
+
+//         3. 观察者开始订阅
+                // 注：此时，才会调用defer（）创建被观察者对象（Observable）
+                observable.subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        Log.d(TAG, "接收到的整数是"+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
+    }
+
+    private void doFromIterableSubscribe() {
+        /*
+         * 集合遍历
+         **/
+        // 1. 设置一个集合
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // 2. 通过fromIterable()将集合中的对象 / 数据发送出去
+        Observable.fromIterable(list)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "集合遍历");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        Log.d(TAG, "集合中的数据元素 = "+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "遍历结束");
+                    }
+                });
+    }
+
+    private void doFromArraySubscribe() {
+        // 1. 设置需要传入的数组
+        Integer[] items = { 0, 1, 2, 3, 4 };
+
+        // 2. 创建被观察者对象（Observable）时传入数组
+        // 在创建后就会将该数组转换成Observable & 发送该对象中的所有数据
+        Observable.fromArray(items)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "数组遍历");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        Log.d(TAG, "数组中的元素 = "+ value  );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "遍历结束");
+                    }
+
+                });
+    }
+
+    private void doJustSubscribe() {
+        Observable.just(1,2,3,4).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "just链式调用-----开始采用subscribe连接");
+            }
+            // 默认最先调用复写的 onSubscribe（）
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "对Next事件"+ value +"作出响应"  );
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        });
     }
 
     private void doSubscribe() {
@@ -43,6 +323,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Log.i(TAG, "doSubscribe: ");
+        
         // 步骤2：创建观察者 Observer 并 定义响应事件行为
 
         Observer<Integer> observer = new Observer<Integer>() {
